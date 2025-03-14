ff56465876d579854ee841a658548ddf5361dbee
HeyAnto <antonin63170@gmail.com>
2025-03-14 23:08:38 +0100

    Add Product Page + Card + Footer

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +11,21 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ShopController extends AbstractController
 {
     #[Route('', name: 'shop_index')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('shop/index.html.twig');
+        $products = $productRepository->findAll();
+
+        $formattedProducts = array_map(function ($product) {
+            return [
+                'title' => $product->getTitle(),
+                'cover' => $product->getCover(),
+                'priceHT' => '€' . number_format($product->getPriceHT(), 2, ',', ' '),
+                'priceTTC' => '€' . number_format($product->getPriceHT() * 1.2, 2, ',', ' '),
+            ];
+        }, $products);
+
+        return $this->render('shop/index.html.twig', [
+            'products' => $formattedProducts
+        ]);
     }
 }
