d5fed898ff854125f746c9adb3f876ce5585e9ae
HeyAnto <antonin63170@gmail.com>
2025-03-17 13:44:46 +0100

    Update

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -3,7 +3,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
+    this.classList.toggle("active-accordion");
 
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
