c7feebd825fa9fd817ceac2dcd9e63c3f41e21bc
HeyAnto <antonin63170@gmail.com>
2025-03-17 14:43:10 +0100

    Update Product Page

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -6,10 +6,12 @@ for (i = 0; i < acc.length; i++) {
     this.classList.toggle("active-accordion");
 
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    if ((panel.style.display === "flex", panel.style.maxHeight)) {
       panel.style.display = "none";
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.display = "flex";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
